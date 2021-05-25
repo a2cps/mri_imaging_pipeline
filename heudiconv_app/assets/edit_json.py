ef152a599ab4baddc90a9ca7838371d934b55c6f
Tanmay Nath <meet10may@gmail.com>
2021-05-24 21:14:16 -0400

    updated instruction

--- heudiconv_app/assets/edit_json.py
+++ heudiconv_app/assets/edit_json.py
@@ -1,7 +1,7 @@
 import sys,json,ast
 from utils import edit_json
 """
-python3 edit_json.py '/scratch/07798/tnath/data/products/development/mris/NS_northshore/bids/NS043021PVP_new_heuristics/sub-043021/ses-PVP/fmap/sub-043021_ses-PVP_acq-dwib0_dir-AP_epi.json' IntendedFor '["ses-PVP/dwi/sub-043021_ses-PVP_dwi.nii.gz","/test"]'
+python3 edit_json.py '/scratch/07798/tnath/data/products/development/mris/NS_northshore/bids/NS043021PVP_new_heuristics/sub-043021/ses-PVP/fmap/sub-043021_ses-PVP_acq-dwib0_dir-AP_epi.json' IntendedFor '["ses-PVP/dwi/sub-043021_ses-PVP_dwi.nii.gz"]'
 """
 try:
     fname = sys.argv[1]
