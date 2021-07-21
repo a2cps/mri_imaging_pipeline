ad0b4725bd3804754f11ce22c6a706ba46a26f04
meet10may <meet10may@gmail.com>
2021-07-21 16:36:02 -0400

    fix:intendedFor sort

--- heudiconv_app/assets/utils.py
+++ heudiconv_app/assets/utils.py
@@ -7,9 +7,9 @@ def create_dwi_b0(dwi_b0_file,dwi_file):
     """
     Creates fieldmaps for DWI data
     """
-    
+
     basepath = str(Path(dwi_b0_file).parents[0])
-    
+
     # load images
     b0_imgs = load_img(dwi_b0_file)
     dwi_imgs = load_img(dwi_file)
@@ -17,7 +17,7 @@ def create_dwi_b0(dwi_b0_file,dwi_file):
     AP = index_img(b0_imgs,[0,1])
     PA = index_img(dwi_imgs,[0,1])
 
-    # Save images as AP and PA. 
+    # Save images as AP and PA.
     # First 2 volumes of b0 are saved as AP
     output_AP_fname = Path(basepath,str(Path(dwi_b0_file).name).replace('dwib0_epi.nii.gz','dwib0_dir-AP_epi.nii.gz'))
     print("Saving AP image as %s"%output_AP_fname)
@@ -27,7 +27,7 @@ def create_dwi_b0(dwi_b0_file,dwi_file):
     output_PA_fname = Path(basepath,str(Path(dwi_b0_file).name).replace('dwib0_epi.nii.gz','dwib0_dir-PA_epi.nii.gz'))
     print("Saving PA image as %s"%output_PA_fname)
     PA.to_filename(output_PA_fname)
-    
+
     return output_AP_fname,output_PA_fname
 
 def create_fmri_b0(b0_file):
@@ -37,12 +37,12 @@ def create_fmri_b0(b0_file):
 
     # Get input path and subject name
     basepath = str(Path(b0_file).parents[0])
-    
-    # Split the fmri b0 image and merge the 1st and 3rd volumes for PA and 2nd and 4th for AP 
+
+    # Split the fmri b0 image and merge the 1st and 3rd volumes for PA and 2nd and 4th for AP
     cmd = 'fslsplit %s %s' %(b0_file,os.path.join(basepath,'vol'))
-    !echo {cmd}
-    !{cmd}
-    
+    print(cmd)
+    os.system(cmd)
+
 #     PA = index_img(b0_imgs,[0,2])
 #     AP = index_img(b0_imgs,[1,3])
 
@@ -51,90 +51,92 @@ def create_fmri_b0(b0_file):
                                                                     'fmrib0_dir-AP_epi.nii.gz'))
     cmd = 'fslmerge -t %s %s %s' %(output_AP_fname,os.path.join(basepath,'vol0001.nii.gz'),
                                os.path.join(basepath,'vol0003.nii.gz'))
-    !echo {cmd}
-    !{cmd}
+    print(cmd)
+    os.system(cmd)
+
     print("Saving AP image as %s"%output_AP_fname)
-    
+
     # Extract the 1st and 3rd volumes of b0 for PA
     output_PA_fname = Path(basepath,str(Path(b0_file).name).replace('fmrib0_epi.nii.gz',
                                                                     'fmrib0_dir-PA_epi.nii.gz'))
-    
+
     cmd = 'fslmerge -t %s %s %s' %(output_PA_fname,os.path.join(basepath,'vol0000.nii.gz'),
                                os.path.join(basepath,'vol0002.nii.gz'))
-    !echo {cmd}
-    !{cmd}
+    print(cmd)
+    os.system(cmd)
+
     print("Saving PA image as %s"%output_PA_fname)
-    
+
     # flip the orientation of AP and save as PA (to make them the same orientation)
     cmd = 'fslswapdim %s x -y z %s' %(output_PA_fname,output_PA_fname)
-    !echo {cmd}
-    !{cmd}
-    
+    print(cmd)
+    os.system(cmd)
+
     # get the sform and qform of swapped image and change the y orientation using fslorient
     PA = load_img(str(output_PA_fname))
-    
+
     sform = PA.header.get_sform()
     qform = PA.header.get_qform()
     sform[1,:] *= -1
     qform[1,:] *= -1
-    
+
     sform = ' '.join(str(v) for v in sform.flatten().tolist())
     qform = ' '.join(str(v) for v in qform.flatten().tolist())
-    
+
     # nipype does not have fslorient command, so have to run it as a shell command!
     cmd = 'fslorient -setsform %s %s' %(sform,output_PA_fname)
     cmd = 'fslorient -setqform %s %s' %(qform,output_PA_fname)
-    !echo {cmd}
-    !{cmd} # for python use os.system(cmd)
-    
+    print(cmd) # for ipython use !echo {cmd} 
+    os.system(cmd) # for ipython use !{cmd} 
+
     files_to_remove = glob.glob(os.path.join(basepath,"vol*"))
     for i in files_to_remove:
-        os.remove(i) 
-        
+        os.remove(i)
+
     return output_AP_fname, output_PA_fname
 
 
 def create_fieldmaps(data_path):
     """
     Creates DWI and fMRI fieldmaps for GE data
-    data_path: str full path of subject 
+    data_path: str full path of subject
     e.g. create_fieldmaps('/home/tanmay/hacking/AC2PC/data/uic/development/UI_uic/UI_travhuman')
     """
     dirs = Path(data_path)
 
     # Accessing subject directory, removing hidden directory and sourcedata
     sub_dir = str(Path(glob.glob(os.path.join(dirs,'sub-*'))[0]))
-    
+
     # Getting session name
     sess_name = os.listdir(sub_dir)[0] # Assuming there is only a single session
-    
+
     # Getting nifti files under fmap directory
     dwi_b0_file = glob.glob(os.path.join(sub_dir,sess_name,'fmap','*dwib0*.nii.gz'))
     dwi_file = glob.glob(os.path.join(sub_dir,sess_name,'dwi','*dwi*.nii.gz'))
     fmri_b0_file = glob.glob(os.path.join(sub_dir,sess_name,'fmap','*fmrib0*.nii.gz'))
-    
+
     # Getting json files under fmap directory
     dwi_json_file = glob.glob(os.path.join(sub_dir,sess_name,'fmap','*dwib0*.json'))
     fmri_json_file = glob.glob(os.path.join(sub_dir,sess_name,'fmap','*fmrib0*.json'))
 
     print("Creating fieldmaps for dwi data...")
     output_AP_fname_dwi,output_PA_fname_dwi = create_dwi_b0(dwi_b0_file[0],dwi_file[0])
-    
+
     print("Creating fieldmaps for fmri data...")
-    output_AP_fname_fmri, output_PA_fname_fmri = create_fmri_b0(fmri_b0_file[0])                   
-    
+    output_AP_fname_fmri, output_PA_fname_fmri = create_fmri_b0(fmri_b0_file[0])
+
     print("Creating json files for DWI data...")
     AP_fname_dwi = str(output_AP_fname_dwi).replace('nii.gz','json')
     PA_fname_dwi = str(output_PA_fname_dwi).replace('nii.gz','json')
     shutil.copyfile(dwi_json_file[0], AP_fname_dwi)
     shutil.copyfile(dwi_json_file[0], PA_fname_dwi)
-    
+
     print("Creating json files for fmri data...")
     AP_fname_fmri = str(output_AP_fname_fmri).replace('nii.gz','json')
     PA_fname_fmri = str(output_PA_fname_fmri).replace('nii.gz','json')
     shutil.copyfile(fmri_json_file[0], AP_fname_fmri)
     shutil.copyfile(fmri_json_file[0], PA_fname_fmri)
-    
+
     # Remove the original fieldmap
     os.remove(str(dwi_b0_file[0]))
     os.remove(str(fmri_b0_file[0]))
@@ -142,19 +144,21 @@ def create_fieldmaps(data_path):
     os.remove(str(dwi_json_file[0]))
 
 
-def add_fields_to_json(json_data, key, pos_key, value):
+def add_fields_to_json(json_data, key, value):
     """
     json_data: json data in the form of dictionary
     key: Name of the key to be added to the json file
-    pos_key: Name of the reference key where the new key is added. The new key is added just above this reference key
     value: Value of the key to be added
     """
-
     new_dict = OrderedDict()
-    for k, v in json_data.items():
-        if k==pos_key:
-            new_dict[key] = value  # insert new key
-        new_dict[k] = v
+    json_data[key]=value
+    # sort the dictionary
+    new_dict = OrderedDict(sorted(json_data.items(), key=lambda t: t[0]))
+    
+    # for k, v in json_data.items():
+    #     if k==pos_key:
+    #         new_dict[key] = value  # insert new key
+    #     new_dict[k] = v
     return new_dict
 
 def save_as_json(data,json_filename):
@@ -192,14 +196,14 @@ def edit_json(data_path):
             philips_scanner=True
             if 'AP' in str(Path(i).name):
                  value = "j-"
-                 json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', 'InstitutionAddress', value)
+                 json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', value)
             else:
                  value="j"
-                 json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', 'InstitutionAddress', value)
+                 json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection',  value)
             print("PhaseEncodingDirection is added to %s"%i)
 
         value = [os.path.join(sess_name,'dwi',dwi_data)]
-        updated_json = add_fields_to_json(json_data, 'IntendedFor', 'InstitutionAddress', value)
+        updated_json = add_fields_to_json(json_data, 'IntendedFor',  value)
         save_as_json(updated_json,i)
         print("IntendedField is added to %s"%i)
         f.close()
@@ -209,7 +213,7 @@ def edit_json(data_path):
         f = open(i,'r')
         json_data=json.load(f)
         value = "j"
-        json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', 'InstitutionAddress', value)
+        json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', value)
         print("PhaseEncodingDirection is added to %s"%i)
         save_as_json(updated_json,i)
         f.close()
@@ -226,15 +230,15 @@ def edit_json(data_path):
         # Adds PhaseEncodingDirection in the json files for Philips scanner
             if 'AP' in str(Path(i).name):
                  value = "j-"
-                 json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', 'InstitutionAddress', value)
+                 json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection',  value)
             else:
                  value="j"
-                 json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', 'InstitutionAddress', value)
+                 json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', value)
             print("PhaseEncodingDirection is added to %s"%i)
 
         # Adds IntendedFor in the json files regardless of any scanner
         value = [os.path.join(sess_name,'func',str(Path(i).name)) for i in func_data]
-        updated_json = add_fields_to_json(json_data, 'IntendedFor', 'InstitutionAddress', value)
+        updated_json = add_fields_to_json(json_data, 'IntendedFor', value)
         save_as_json(updated_json,i)
         print("IntendedFor is added to %s"%i)
         f.close()
@@ -246,14 +250,14 @@ def edit_json(data_path):
            print(i)
            #if 'AP' in str(Path(i).name):
            value = "j"
-           json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', 'InstitutionAddress', value)
+           json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', value)
            #else:
            #      value="j"
-           #      json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', 'InstitutionAddress', value)
+           #      json_data = add_fields_to_json(json_data, 'PhaseEncodingDirection', value)
 
            print("PhaseEncodingDirection is added to %s"%i)
             # Adds sliceTiming for Philips scanner
-           updated_json = add_fields_to_json(json_data, 'SliceTiming', 'SoftwareVersions', slice_timing)
+           updated_json = add_fields_to_json(json_data, 'SliceTiming', slice_timing)
            print("SliceTiming is added to %s"%i)
            save_as_json(updated_json,i)
            f.close()
