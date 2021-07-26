a5a132480532743f238fbc6719c829e09ca85f59
meet10may <meet10may@gmail.com>
2021-07-26 14:29:52 -0400

    add:trigger-deletion

--- heudiconv_app/assets/utils.py
+++ heudiconv_app/assets/utils.py
@@ -1,7 +1,75 @@
-import os,json,glob
+import os,json,glob,pydicom,shutil
 from pathlib import Path
 from nilearn.image import load_img,index_img
 from collections import OrderedDict
+
+def make_copy(path):
+    """
+    Makes a copy of the original data. The original data is saved with a suffix "_orig"
+    """
+    suffix = '-orig'
+    dst = os.path.join(path+suffix)
+    # dst = os.path.join(path,'dicom')
+    print("Making a copy of the data...")
+    if os.path.isdir(dst):
+        flag=True
+        print("Destination directory already exists")
+    else:
+        shutil.copytree(path, dst)
+        flag=False
+        print("Done!The original copy is %s"%dst)
+    return dst,flag
+
+def get_subdirectory(path):
+    """
+    Returns all the subdirectories under 'func' directory of the raw subject data.
+    """
+    dirs = []
+    for dirpath, dirnames, filenames in os.walk(path):
+        if not dirnames:
+            dirs.append(dirpath)
+    return dirs,filenames,dirnames
+
+def delete_tag(fname):
+    try:
+        ds = pydicom.read_file(fname)
+        if ds.__contains__('TriggerTime'):
+        # Delete the dicom tag 0018,1060. This tag represents the Trigger value
+            del(ds['0018','1060'])
+        else:
+            print("No trigger tag found for %s"%fname)
+        ds.save_as(fname)
+    except:
+        print("Unable to open the file %s"%fname)
+
+
+def edit_dicom_file_philips(filepath):
+    """
+    Deletes the trigger tag (0018,1060) from the DICOM file.
+    """
+    # Copy the data, add suffix "_orig" to the original data and return the path of duplicate data
+    new_path,flag  = make_copy(filepath)
+
+    if not flag:
+        dirs,files,dirnames = get_subdirectory(new_path)
+        for func in dirs:
+            print("Deleting tag for %s"%func)
+            for i in sorted(os.listdir(func)):
+                fname = os.path.join(func,i)
+                print('Working on file %s'%fname)
+                delete_tag(fname)
+
+        print("Searching for any files under %s"%filepath)
+
+        if files !=[]:
+            for i in sorted(files):
+                fname = os.path.join(filepath,i)
+                print('Working on file %s'%fname)
+                delete_tag(fname)
+        print("Done! New dicoms are stored in %s"%os.path.join(new_path))
+    else:
+        print("Skipping!")
+
 
 def create_dwi_b0(dwi_b0_file,dwi_file):
     """
