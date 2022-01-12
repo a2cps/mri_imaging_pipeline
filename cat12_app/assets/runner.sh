28506591cbad52739921e276541f51a81c0990c0
Patrick Sadil <psadil@gmail.com>
2022-01-12 07:33:43 -0500

    --amend

--- cat12_app/assets/runner.sh
+++ cat12_app/assets/runner.sh
@@ -1,27 +1,26 @@
-# Allow over-ride
-if [ -z "${CONTAINER_IMAGE}" ]
-then
-    version=$(cat ./_util/VERSION)
-    CONTAINER_IMAGE="index.docker.io/library/ubuntu:bionic"
-fi
-. lib/container_exec.sh
+#!/usr/bin/env bash
+set -x
 
-# Write an excution command below that will run a script or binary inside the 
-# requested container, assuming that the current working directory is 
-# mounted in the container as its WORKDIR. In place of 'docker run' 
-# use 'container_exec' which will handle setup of the container on 
-# a variety of host environments. 
-#
-# Here is a template:
-#
-# container_exec ${CONTAINER_IMAGE} COMMAND OPTS INPUTS
-#
-# Here is an example of counting words in local file 'poems.txt',
-# outputting to a file 'wc_out.txt'
-#
-# container_exec ${CONTAINER_IMAGE} wc poems.txt > wc_out.txt
-#
-
-# set -x
-
-# set +x
+singularity exec \
+        -B "${BIDS_DIRECTORY}":"${BIDS_DIRECTORY}" \
+        -B "${BIND_DIR}":"${BIND_DIR}" \
+        --cleanenv \
+        docker://${CONTAINER_IMAGE} \
+        mriqc \
+        ${BIDS_DIRECTORY} \
+        ${OUTPUT_DIR} \
+        participant --participant-label ${PARTICIPANT_LABEL} \
+        ${WORK_DIR} \
+        --no-sub \
+        --n_procs 50 \
+        --mem_gb 180 \
+        ${ICA} \
+        ${STOP_IDX} \
+        ${START_IDX} \
+        ${FFT_SPIKES} \
+        ${WRITE_GRAPH} \
+        ${CORRECT_SLICE_TIMING} \
+        ${FD_THRESHOLD} \
+        ${TASK_ID} \
+        ${MODALITIES} \
+        --verbose-reports
