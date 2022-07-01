08a61830a82fe01241c4a09aa63910849cef360b
Patrick Sadil <psadil@gmail.com>
2022-07-01 20:16:30 +0000

    Revert "Fix/rotated fmaps"

--- heudiconv_app/assets/edit_json.py
+++ heudiconv_app/assets/edit_json.py
@@ -1,6 +1,5 @@
-import sys
+import sys,json,ast
 from utils import edit_json
-
 """
 python3 edit_json.py /scratch/07798/tnath/data/products/development/mris/NS_northshore/bids/NS043021PVP_new_heuristics/
 """
