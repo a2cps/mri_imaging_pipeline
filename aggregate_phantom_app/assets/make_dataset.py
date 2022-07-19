bc22e583e90a57c789ed38752d84b4872d7db163
Patrick Sadil <psadil@gmail.com>
2022-07-19 12:17:11 -0400

    add pem for authentication to confluence

--- aggregate_phantom_app/assets/make_dataset.py
+++ aggregate_phantom_app/assets/make_dataset.py
@@ -10,7 +10,7 @@ SITE_LONG = {
     "UC": "UC_uchicago",
     "UM": "UM_umichigan",
     "SH": "SH_spectrum_health",
-    "WS": "WS_wayne_state"
+    "WS": "WS_wayne_state",
 }
 
 BIDS_IGNORE = """
@@ -21,38 +21,36 @@ __pycache__*\n
 
 DESCRIPTION = {
     "Acknowledgements": "TODO: more",
-    "Authors": [
-        "TODO:"
-    ],
+    "Authors": ["TODO:"],
     "BIDSVersion": "1.9.3",
     "DatasetDOI": "TODO: eventually a DOI for the dataset",
-    "Funding": [
-        "TODO"
-    ],
+    "Funding": ["TODO"],
     "HowToAcknowledge": "TODO: describe how to acknowledge -- either cite a corresponding paper, or just in acknowledgement section",
     "License": "TODO: choose a license, e.g. PDDL (http://opendatacommons.org/licenses/pddl/)",
     "Name": "TODO: name of the dataset",
-    "ReferencesAndLinks": [
-        "TODO"
-    ]
+    "ReferencesAndLinks": ["TODO"],
 }
 
 README = "phantom dataset"
 
 
-def get_valid(inroot = pathlib.Path) -> list[pathlib.Path]:
+def get_valid(inroot=pathlib.Path) -> list[pathlib.Path]:
     passed = []
     for site in SITE_LONG.values():
         # append only if the *out file is present in the folder
-        passed += [ d for d in inroot.glob(f"{site}/bids_validation/*QC*") if len([x for x in d.glob('*out')]) > 0 ]
+        passed += [
+            d
+            for d in inroot.glob(f"{site}/bids_validation/*QC*")
+            if len([x for x in d.glob("*out")]) > 0
+        ]
 
     return passed
 
 
 def main(
-    outdir: pathlib.Path, 
-    inroot: pathlib.Path = pathlib.Path("/corral-secure/projects/A2CPS/products/mris")
-    ) -> None:
+    outdir: pathlib.Path,
+    inroot: pathlib.Path = pathlib.Path("/corral-secure/projects/A2CPS/products/mris"),
+) -> None:
 
     if not outdir.exists():
         outdir.mkdir(parents=True, exist_ok=True)
@@ -64,7 +62,9 @@ def main(
                 target = outdir / phantom_id.name / ses.name
                 if not target.exists() or ses.stat().st_mtime > target.stat().st_mtime:
                     print(f"copying {phantom_id.absolute()} -> {target.absolute()}")
-                    shutil.copytree(ses.absolute(), target.absolute(), dirs_exist_ok=True)
+                    shutil.copytree(
+                        ses.absolute(), target.absolute(), dirs_exist_ok=True
+                    )
                 else:
                     print(f"skipping {phantom_id.absolute()}")
 
@@ -79,19 +79,15 @@ def main(
     bids_ignore.write_text(BIDS_IGNORE)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('outdir', type=pathlib.Path)
+    parser.add_argument("outdir", type=pathlib.Path)
     parser.add_argument(
-        '--inroot', 
-        type=pathlib.Path, 
-        default=pathlib.Path("/corral-secure/projects/A2CPS/products/mris"))
-
-    args = parser.parse_args()
-    main(
-        inroot=args.inroot,
-        outdir=args.outdir
+        "--inroot",
+        type=pathlib.Path,
+        default=pathlib.Path("/corral-secure/projects/A2CPS/products/mris"),
     )
 
-
+    args = parser.parse_args()
+    main(inroot=args.inroot, outdir=args.outdir)
