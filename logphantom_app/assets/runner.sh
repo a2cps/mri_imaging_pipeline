6b41361856c7792be9d17ace7b03543188982111
Patrick Sadil <psadil@gmail.com>
2022-08-04 15:23:30 -0400

    replace tsv log with sqlite database

--- logphantom_app/assets/runner.sh
+++ logphantom_app/assets/runner.sh
@@ -5,9 +5,16 @@ set -x
 date
 
 # shellcheck disable=SC2086
-singularity exec \
+singularity run \
     --cleanenv \
     -B "${BIND_DIR}":"${BIND_DIR}" \
-    "${CONTAINER_IMAGE}" \
-    python log.py --bidsroot "${BIDS}" --products "${PRODUCTS}" ${POST}
+    ${CONTAINER_IMAGE} \
+    init ${PRODUCTS} ${URL}
+
+# shellcheck disable=SC2086
+singularity run \
+    --cleanenv \
+    -B "${BIND_DIR}":"${BIND_DIR}" \
+    ${CONTAINER_IMAGE} \
+    write-and-post ${URL} ${POST} 
 
