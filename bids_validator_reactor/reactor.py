31c81d800f96f379d67c7ee08b4ba57d0ca8c40c
Patrick Sadil <psadil@gmail.com>
2022-08-29 13:17:02 -0400

    handle archiveapath in qaphantom_app

--- bids_validator_reactor/reactor.py
+++ bids_validator_reactor/reactor.py
@@ -96,11 +96,7 @@ def submit_bids_validate(r, bids, filename, subject_id, site):
             {
                 "event": "FINISHED",
                 "persistent": False,
-                "url": qaphantom_callback 
-                + "&status=${JOB_STATUS}" 
-                + "&bids=" + bids 
-                + "&site="+ site
-                + "&filename=" _get_output_dir(bids)
+                "url": qaphantom_callback + "&status=${JOB_STATUS}&bids=" + bids,
             }
         ]
     else:
@@ -157,7 +153,7 @@ def submit_bids_validate(r, bids, filename, subject_id, site):
                 + bids
                 + "&filename="
                 + _get_output_dir(bids),
-            }
+            },
         ]
         job_def.notifications = notif
 
