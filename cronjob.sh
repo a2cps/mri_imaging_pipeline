906cab529ce951301fedeffc3d5a97f58a0d76c0
Patrick Sadil <psadil@gmail.com>
2022-10-20 16:39:24 -0400

    Closes #104

--- cronjob.sh
+++ cronjob.sh
@@ -28,20 +28,26 @@ function skip_file() {
 }
 
 
+# most received DICOMs follow this pattern
+zips=("$indir"/*/*/*.zip)
+# but WS sends two sets of phantom dicoms:
+#   $indir/Wayne_state/QA/DSV
+#   $indir/Wayne_state/QA/BIRD
+# we only want to process the DSV files. 
+zips+=("$indir"/Wayne_state/QA/DSV/*zip)
 
 touch "$submitted"
-/bin/ls "$indir"/*/*/*.zip | while read f; do
+for f in "${zips[@]}"; do
 	if grep -q "^$f\$" "$submitted"; then
 		echo "$f was submitted, skipping"
 		continue
 	fi
 	#cd "$(dirname $f)";
 	#if ! md5sum -c "$f"; then
-	if test 'find "$F" -mmin +10'
-	then
+	if test 'find "$F" -mmin +10';  then
 		#zipfile="${f%.MD5SUM}"
-		zipfile=${f}
-		filename=$(basename "$zipfile" | sed -e 's,.zip,,g')
+		# zipfile=${f}
+		# filename=$(basename "$zipfile" | sed -e 's,.zip,,g')
 		#subj=${filename%_*}
 		#ses=${filename#*_}
 		#site="$(dirname $zipfile| xargs basename)"
@@ -50,14 +56,13 @@ touch "$submitted"
 		#outdir="$site_path/bids/$subj"
 		# requires tapis from tapis-cli (pypi)	
 		#echo tapis actors submit -m "{\"site\": \"$site\", \"subject\": \"$subj\", \"session\": \"$ses\", \"zipfile\": \"$zipfile\", \"outdir\": \"$outdir\"}" "$heudiconv_actorid"
-		echo tapis actors submit -m "{\"zipfile\": \"$zipfile\"}" "$heudiconv_actorid"
-		tapis actors submit -m "{\"zipfile\": \"$zipfile\"}" $heudiconv_actorid
+		echo tapis actors submit -m "{\"zipfile\": \"$f\"}" "$heudiconv_actorid"
+		tapis actors submit -m "{\"zipfile\": \"$f\"}" $heudiconv_actorid
 		echo "$f" >> "$submitted"
-		abaco submit -m "{\"text\": \"detected and submitted for processing: \"$zipfile\"}" $notifications_id
+		abaco submit -m "{\"text\": \"detected and submitted for processing: \"$f\"}" $notifications_id
 		
-		continue
 	else 
 		skip_file "modified within 10 minutes"
-		fi
+	fi
 
 done
