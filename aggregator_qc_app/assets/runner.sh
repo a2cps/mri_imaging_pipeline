dd6b84abfb1b08b6c474f143c61c646c30c39964
Patrick Sadil <psadil@gmail.com>
2023-02-02 14:17:34 -0500

    delete broken symlinks and empty directories

--- aggregator_qc_app/assets/runner.sh
+++ aggregator_qc_app/assets/runner.sh
@@ -16,6 +16,13 @@ for site in ${SITES}; do
 
 done
 
+# remove any symlinks that are broken
+# (e.g., a subject was deleted after renaming)
+find "${OUTDIR}" -xtype l -delete
+
+# prune empty directories
+find "${OUTDIR}" -type d -empty -delete
+
 set -e
 # since bids and mriqc files are only symlinks, the targets of the 
 # symlinks must also be bound (hence -B for INROOT)
