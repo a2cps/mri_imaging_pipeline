f2663f002411de30ddc49c6299bb758a867fc596
Micah Johnson <micah.alan.johnson@gmail.com>
2023-02-14 13:35:03 -0800

    Test from Micah Johnson

--- fmriprep_app/assets/runner-template.sh
+++ fmriprep_app/assets/runner-template.sh
@@ -54,3 +54,4 @@ singularity exec \
         ${BIDS_FILTER_FILE} ${FS_NO_RECONALL} ${FS_SUBJECTS_DIR} ${SKIP_BIDS_VALIDATION} \
         --fs-license-file /opt/freesurfer_license/license.txt
 
+find “${OUTDIR}” -name “*nii.gz” -exec gunzip -t {} \+
