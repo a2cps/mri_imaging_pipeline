ab1f77c9307d51abeb9edde60ed66af3d90b8f7c
urrutia <jurrutia@tacc.utexas.edu>
2023-05-23 09:41:29 -0500

    updates to dicom app for tapis3

--- dicom_reader_app/assets/runner-template.sh
+++ dicom_reader_app/assets/runner-template.sh
@@ -1,16 +1,4 @@
-# Import Agave runtime extensions
-. _lib/extend-runtime.sh
+#!/bin/bash
 
-
-echo Input is ${FILENAME}
-
-# Echo command to std out
-echo singularity run \
-            -B /corral-secure/projects/A2CPS/:/corral-secure/projects/A2CPS/ \
-            docker://${CONTAINER_IMAGE} \
-            python file_check.py ${FILENAME} ${SUBJECT_ID}
-
-singularity run \
-            -B /corral-secure/projects/A2CPS/:/corral-secure/projects/A2CPS/ \
-            docker://${CONTAINER_IMAGE} \
-            python file_check.py ${FILENAME} ${SUBJECT_ID}
+echo /opt/conda/bin/python /tapis/assets/file_check.py $1 $2
+/opt/conda/bin/python /tapis/assets/file_check.py $1 $2
