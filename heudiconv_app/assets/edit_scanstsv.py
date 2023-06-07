a0b6dfd82b1749f4e01323617d896071707297ff
Patrick Sadil <psadil@gmail.com>
2023-06-07 09:21:41 -0400

    remove operator and randstr columns from scans.tsv

---
+++ heudiconv_app/assets/edit_scanstsv.py
@@ -0,0 +1,19 @@
+import argparse
+from pathlib import Path
+
+import pandas as pd
+
+
+def main(bidsdir: Path) -> None:
+    for scans_tsv in bidsdir.rglob("*scans.tsv"):
+        scans = pd.read_csv(scans_tsv, delim_whitespace=True)
+        scans.drop(columns=["operator", "randstr"], inplace=True)
+        scans.to_csv(scans_tsv, sep="\t", na_rep="n/a", index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bidsdir", type=Path)
+
+    args = parser.parse_args()
+    main(bidsdir=args.bidsdir)
