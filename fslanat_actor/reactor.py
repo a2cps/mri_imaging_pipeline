9835d1b1fd7c8635c37882838266435789651591
Patrick Sadil <psadil@gmail.com>
2023-07-11 10:16:27 -0400

    pin packages

--- fslanat_actor/reactor.py
+++ fslanat_actor/reactor.py
@@ -16,8 +16,6 @@ from ibis.expr.types.relations import Table
 from tapipy import actors, util, errors
 from tapipy.tapis import Tapis
 
-ibis.set_backend("pandas")
-
 # within docker container
 JOB = Path("/opt/job.json")
 # on TACC
