ca7fd9c7bbbde5c60f1004726a6ed60933a8fc40
urrutia <jurrutia@tacc.utexas.edu>
2023-08-22 13:19:19 -0400

    removing space from path

--- fslanat_actor/reactor.py
+++ fslanat_actor/reactor.py
@@ -20,7 +20,7 @@ from tapipy.tapis import Tapis
 JOB = Path("/opt/job.json")
 # on TACC
 #ILOG = "/corral-secure/projects/A2CPS/community/reports/imaging/imaging-log-latest.csv"
-ILOG = " /corral-secure/projects/A2CPS/system/cronjob/imaging_report/report.csv"
+ILOG = "/corral-secure/projects/A2CPS/system/cronjob/imaging_report/report.csv"
 
 # can be overriden by incoming message
 _MAXJOBS = 80
