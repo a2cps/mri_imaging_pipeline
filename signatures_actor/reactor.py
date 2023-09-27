7e577a8939ebd9a8cb713e87e3c579f389f6db83
urrutia <jurrutia@tacc.utexas.edu>
2023-09-27 11:22:54 -0400

    updating number of subjects for signatures and fcn

--- signatures_actor/reactor.py
+++ signatures_actor/reactor.py
@@ -26,7 +26,7 @@ JOB = Path("/opt/job.json")
 ILOG = "/corral-secure/projects/A2CPS/system/cronjob/imaging_report/report.csv"
 
 # can be overriden by incoming message
-_MAXJOBS = 20
+_MAXJOBS = 50
 
 SITE_LONG = {
     "NS": "NS_northshore",
