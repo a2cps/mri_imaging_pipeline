0f1e735799688df85a82622e326bd1e67426fefe
Patrick Sadil <psadil@gmail.com>
2023-10-03 12:16:27 -0400

    allow spreading of snapshot across jobs

--- snapshot_app/assets/entrypoint.py
+++ snapshot_app/assets/entrypoint.py
@@ -15,20 +15,29 @@ logging.basicConfig(
 
 
 def main(
-    inroot: Path, outroot: Path, riadir: Path, n_workers: int = 1
+    inroot: Path,
+    outroot: Path,
+    riadir: Path,
+    n_workers: int = 1,
+    copy: bool = False,
+    init_datalad: bool = False,
+    archive: bool = False,
 ) -> None:
-    logging.info("making initial copy")
-    copy_v1_to_dst_wf.main(inroot=inroot, outroot=outroot)
+    if copy:
+        logging.info("making initial copy")
+        copy_v1_to_dst_wf.main(inroot=inroot, outroot=outroot)
 
-    logging.info("initializing datalad")
-    init_datalad_wf.main(inroot=outroot, n_jobs=n_workers)
+    if init_datalad:
+        logging.info("initializing datalad")
+        init_datalad_wf.main(inroot=outroot, n_jobs=n_workers)
 
-    ria = f"ria+file://{riadir.resolve()}"
-    logging.info(f"configuring ria at {ria=}")
-    add_ria_wf.main(releasedir=outroot, ria=ria)
+    if archive:
+        ria = f"ria+file://{riadir.resolve()}"
+        logging.info(f"configuring ria at {ria=}")
+        add_ria_wf.main(releasedir=outroot, ria=ria)
 
-    logging.info("archiving to ria")
-    archive_wf.main(releasedir=outroot, ria=riadir, n_jobs=n_workers)
+        logging.info("archiving to ria")
+        archive_wf.main(releasedir=outroot, ria=riadir, n_jobs=n_workers)
 
 
 if __name__ == "__main__":
@@ -36,6 +45,15 @@ if __name__ == "__main__":
     parser.add_argument("--inroot", type=Path, required=True)
     parser.add_argument("--outroot", type=Path, required=True)
     parser.add_argument("--riadir", type=Path, required=True)
+    parser.add_argument(
+        "--copy", action=argparse.BooleanOptionalAction, default=False
+    )
+    parser.add_argument(
+        "--init-datalad", action=argparse.BooleanOptionalAction, default=False
+    )
+    parser.add_argument(
+        "--archive", action=argparse.BooleanOptionalAction, default=False
+    )
     parser.add_argument("--n-workers", type=int, default=1)
 
     args = parser.parse_args()
