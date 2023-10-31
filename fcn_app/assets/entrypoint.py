b56eafc3d5494ae3bf9ec94da1711a88cbf68c24
Patrick Sadil <psadil@gmail.com>
2023-10-31 07:40:03 -0400

    wip

--- fcn_app/assets/entrypoint.py
+++ fcn_app/assets/entrypoint.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 import os
 import shutil
+import tempfile
 from pathlib import Path
 
 from functional_connectivity.cli import functional_connectivity
@@ -17,31 +18,40 @@ def main(
         msg = "Unable to find expected log file: tapisjob.out"
         raise AssertionError(msg)
 
-    # fmriprep dirs may point to broken symlinks, or folders might not exist
-    # so, need to ensure that we get one output dir for each input dir
-    fmriprep_subdirs = []
-    output_dir_final = []
-    for ind, outd in zip(fmriprep_dir, output_dir, strict=True):
-        logging.info(f"Looking for sub dirs in {ind}")
-        for d in ind.glob("sub*"):
-            if d.is_dir():
-                logging.info(f"Found! Will process files in {d}")
-                fmriprep_subdirs.append(d)
-                output_dir_final.append(outd)
+    # use faster filesystem for workflow, which may involve parallel writes
+    with tempfile.TemporaryDirectory() as tmpd_:
+        tmpd = Path(tmpd_)
+
+        # fmriprep dirs may point to broken symlinks, or folders might not exist
+        # so, need to ensure that we get one output dir for each input dir
+        fmriprep_subdirs: list[Path] = []
+        output_dir_tmp: list[Path] = []
+        for ind, outd in zip(fmriprep_dir, output_dir, strict=True):
+            logging.info(f"Looking for sub dirs in {ind}")
+            for d in ind.glob("sub*"):
+                if d.is_dir():
+                    logging.info(f"Found! Will process files in {d}")
+                    fmriprep_subdirs.append(d)
+                    output_dir_tmp.append(tmpd / outd)
+                else:
+                    logging.warning(
+                        f"No valid sub directories found within {ind}"
+                    )
+
+        functional_connectivity._main(
+            fmriprep_subdirs=fmriprep_subdirs,
+            output_dirs=output_dir_tmp,
+            n_workers=n_workers,
+        )
+
+        for o in set(output_dir_tmp):
+            if o.exists():
+                shutil.copy2(oldlog, o / f"{uuid}.out")
+                shutil.copytree(o, o.relative_to(tmpd))
             else:
-                logging.warning(f"No valid sub directories found within {ind}")
-
-    functional_connectivity._main(
-        fmriprep_subdirs=fmriprep_subdirs,
-        output_dirs=output_dir_final,
-        n_workers=n_workers,
-    )
-
-    for o in output_dir:
-        if o.exists():
-            shutil.copy2(oldlog, o / f"{uuid}.out")
-        else:
-            logging.warning(f"Expected {o} but that path does not exist")
+                logging.warning(
+                    f"Expected {o.relative_to(tmpd)} but that path does not exist"
+                )
 
 
 if __name__ == "__main__":
