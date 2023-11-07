db6e3b286765d9d44bd89f564aff559cc6cf2a3f
Patrick Sadil <psadil@gmail.com>
2023-11-07 08:25:33 -0500

    update RU jsons with final expected values

--- heudiconv_app/assets/add_date_to_xa30.py
+++ heudiconv_app/assets/add_date_to_xa30.py
@@ -7,17 +7,21 @@ import pydicom
 
 
 def main(dicomdir: Path, bidsdir: Path) -> None:
-    for dcm in dicomdir.rglob("**/*"):
+    for dcm in dicomdir.rglob("*"):
         if dcm.is_file():
             try:
-                header = pydicom.dcmread(dcm, specific_tags=["SeriesDate", "SeriesTime"])
+                header = pydicom.dcmread(
+                    dcm, specific_tags=["SeriesDate", "SeriesTime"]
+                )
                 AcquisitionDateTime = datetime.strptime(
                     header.SeriesDate + header.SeriesTime, "%Y%m%d%H%M%S.%f"
                 ).isoformat()
                 for scans_tsv in bidsdir.glob("**/sub*scans.tsv"):
                     scans = pd.read_csv(scans_tsv, delim_whitespace=True)
                     scans.acq_time = AcquisitionDateTime
-                    scans.to_csv(scans_tsv, sep="\t", na_rep="n/a", index=False)
+                    scans.to_csv(
+                        scans_tsv, sep="\t", na_rep="n/a", index=False
+                    )
                 break
             except Exception:
                 pass
