4de594ad8877e69622bd0f282db5f14cafad2a4a
Patrick Sadil <psadil@gmail.com>
2024-02-20 12:31:39 -0500

    update sample test

--- qsirecon-fsl_dtifit_app/assets/entrypoint.sh
+++ qsirecon-fsl_dtifit_app/assets/entrypoint.sh
@@ -1,46 +1,47 @@
 #!/usr/bin/env bash
 
-# run once to confirm that the image has been cached (otherwise, may be downloaded by each job)
-singularity run --cleanenv "${container_image}" --help
+main (){
+    local BIDSDIR="${1}"
+    local WORKDIR="${2}"
+    local RECON_OUTDIR="${3}"
+    local RECON_ONLY="${4}"
+    local RECON_SPEC="${5}"
+    local QSIPREP_DIR="${6}"
+    local PARTICIPANT_LABEL="${7}"
+    local FREESURFER_DIR="${8}"
+    local NTHREADS="${9}"
+    local MEMMB="${10}"
+    local OUTPUT_RESOLUTION="${11}"
 
-# run the qsiprep_recon step
-singularity run \
--e \
---bind "${BIND_DIR}":"${BIND_DIR}" \
-"${CONTAINER}" \
-"${BIDSDIR}" \
-"${RECON_OUTDIR}" \
-"${WORKDIR}" \
-"${RECON_ONLY}" \
-"${RECON_SPEC}" \
-"${QSIPREP_DIR}" \
-"${PARTICIPANT_LABEL}" \
-"${FREESURFER_DIR}" \
-"${NTHREADS}" \
-"${MEMMB}" \
-"${OUTPUT_RESOLUTION}"
+    qsiprep \
+        --participant_label "${PARTICIPANT_LABEL}" \
+        --work-dir "${WORKDIR}" \
+        "${BIDSDIR}"
+    # remaining arguments here
 
-## "${RECON_OUTPUT}"
-## e.g., /corral-secure/projects/A2CPS/products/mris/${site}/qsirecon-fsl/${subj}
+    ## How to define $subj and $ses??
 
+    local recon_dir="${RECON_OUTPUT}"/qsirecon/sub-${subj}/ses-${ses}/dwi
 
-## How to define $subj and $ses??
+    local data=$recon_dir/sub-${subj}_ses-${ses}_space-T1w_desc-preproc_fslstd_dwi.nii.gz
+    local mask=$recon_dir/sub-${subj}_ses-${ses}_space-T1w_desc-preproc_fslstd_mask.nii.gz
+    local bvecs=$recon_dir/sub-${subj}_ses-${ses}_space-T1w_desc-preproc_fslstd_dwi.bvec
+    local bvals=$recon_dir/sub-${subj}_ses-${ses}_space-T1w_desc-preproc_fslstd_dwi.bval
 
-recon_dir="${RECON_OUTPUT}"/qsirecon/sub-${subj}/ses-${ses}/dwi
+    ## "DTIFIT_OUTPUT_OLS"
+    ## e.g., /corral-secure/projects/A2CPS/products/mris/${site}/dtifit_ols/sub-${subj}/ses-${ses}/sub-${subj}_ses-${ses}
 
-data=$recon_dir/sub-${subj}_ses-${ses}_space-T1w_desc-preproc_fslstd_dwi.nii.gz
-mask=$recon_dir/sub-${subj}_ses-${ses}_space-T1w_desc-preproc_fslstd_mask.nii.gz
-bvecs=$recon_dir/sub-${subj}_ses-${ses}_space-T1w_desc-preproc_fslstd_dwi.bvec
-bvals=$recon_dir/sub-${subj}_ses-${ses}_space-T1w_desc-preproc_fslstd_dwi.bval
-
-
-## "DTIFIT_OUTPUT_OLS"
-## e.g., /corral-secure/projects/A2CPS/products/mris/${site}/dtifit_ols/sub-${subj}/ses-${ses}/sub-${subj}_ses-${ses}
-
-dtifit -k ${data} -o "${DTIFIT_OUTPUT_OLS}" -m ${mask} -r ${bvecs} -b ${bvals} --ols --sse --save_tensor
+    dtifit -k ${data} -o "${DTIFIT_OUTPUT_OLS}" -m ${mask} -r ${bvecs} -b ${bvals} --ols --sse --save_tensor
 
 
-## "DTIFIT_OUTPUT_WLS"
-## e.g., /corral-secure/projects/A2CPS/products/mris/${site}/dtifit_wls/sub-${subj}/ses-${ses}/sub-${subj}_ses-${ses}
-dtifit -k ${data} -o "${DTIFIT_OUTPUT_WLS}" -m ${mask} -r ${bvecs} -b ${bvals} --wls --sse --save_tensor
+    ## "DTIFIT_OUTPUT_WLS"
+    ## e.g., /corral-secure/projects/A2CPS/products/mris/${site}/dtifit_wls/sub-${subj}/ses-${ses}/sub-${subj}_ses-${ses}
+    dtifit -k ${data} -o "${DTIFIT_OUTPUT_WLS}" -m ${mask} -r ${bvecs} -b ${bvals} --wls --sse --save_tensor
+
+
+}
+export -f main
+
+main "$@"
+
 
