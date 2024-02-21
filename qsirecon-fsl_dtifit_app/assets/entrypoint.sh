b42ce4d50d1c059ee3f815f439d3acd2ec0bd676
Patrick Sadil <psadil@gmail.com>
2024-02-21 15:44:30 -0500

    docker walkthrough dev

--- qsirecon-fsl_dtifit_app/assets/entrypoint.sh
+++ qsirecon-fsl_dtifit_app/assets/entrypoint.sh
@@ -9,13 +9,11 @@ main (){
     local PARTICIPANT_LABEL="${6}"
     local NTHREADS="${7}"
     local MEMMB="${8}"
+    local LICENSE="${9}"
 
     ## RUN QSIRECON
     qsiprep \
-        participant \
         --participant_label "${PARTICIPANT_LABEL}" \
-        "${BIDSDIR}" \
-        "${OUTDIR}" \
         --work-dir "${WORKDIR}" \
         --recon-only \
         --recon_spec reorient_fslstd \
@@ -23,24 +21,28 @@ main (){
         --freesurfer-input "${FREESURFERDIR}" \
         --output-resolution 1.7 \
         --nthreads "${NTHREADS}" \
-        --mem_mb "${MEMMB}"
+        --mem_mb "${MEMMB}" \
+        --fs-license-file "${LICENSE}" \
+        "${BIDSDIR}" "${OUTDIR}" participant
+    
+    echo "qsiprep recon finished!"
 
-    ## DEFINE ARGS FOR DTIFIT
-    local qsirecon_dir="${OUTDIR}"/qsirecon/"${PARTICIPANT_LABEL}"/dwi
-        ## check, does qsirecon outpout for traveling data include "_ses-" by default?
-    local data="${qsirecon_dir}"/"${PARTICIPANT_LABEL}"_space-T1w_desc-preproc_fslstd_dwi.nii.gz
-    local mask="${qsirecon_dir}"/"${PARTICIPANT_LABEL}"_space-T1w_desc-preproc_fslstd_mask.nii.gz
-    local bvecs="${qsirecon_dir}"/"${PARTICIPANT_LABEL}"_space-T1w_desc-preproc_fslstd_dwi.bvec
-    local bvals="${qsirecon_dir}"/"${PARTICIPANT_LABEL}"_space-T1w_desc-preproc_fslstd_dwi.bval
+    # ## DEFINE ARGS FOR DTIFIT
+    # local qsirecon_dir="${OUTDIR}"/qsirecon/"${PARTICIPANT_LABEL}"/dwi
+    #     ## check, does qsirecon outpout for traveling data include "_ses-" by default?
+    # local data="${qsirecon_dir}"/"${PARTICIPANT_LABEL}"_space-T1w_desc-preproc_fslstd_dwi.nii.gz
+    # local mask="${qsirecon_dir}"/"${PARTICIPANT_LABEL}"_space-T1w_desc-preproc_fslstd_mask.nii.gz
+    # local bvecs="${qsirecon_dir}"/"${PARTICIPANT_LABEL}"_space-T1w_desc-preproc_fslstd_dwi.bvec
+    # local bvals="${qsirecon_dir}"/"${PARTICIPANT_LABEL}"_space-T1w_desc-preproc_fslstd_dwi.bval
 
-    local DTIFIT_OUTDIR_OLS="${OUTDIR}"/dtifit/ols
-    local DTIFIT_OUTDIR_WLS="${OUTDIR}"/dtifit/wls
+    # local DTIFIT_OUTDIR_OLS="${OUTDIR}"/dtifit/ols
+    # local DTIFIT_OUTDIR_WLS="${OUTDIR}"/dtifit/wls
 
-    ## RUN DTIFIT (OLS)
-    dtifit -k ${data} -o "${DTIFIT_OUTDIR_OLS}" -m ${mask} -r ${bvecs} -b ${bvals} --ols --sse --save_tensor
+    # ## RUN DTIFIT (OLS)
+    # dtifit -k ${data} -o "${DTIFIT_OUTDIR_OLS}" -m ${mask} -r ${bvecs} -b ${bvals} --ols --sse --save_tensor
 
-    ## RUN DTIFIT (WLS)
-    dtifit -k ${data} -o "${DTIFIT_OUTDIR_WLS}" -m ${mask} -r ${bvecs} -b ${bvals} --wls --sse --save_tensor
+    # ## RUN DTIFIT (WLS)
+    # dtifit -k ${data} -o "${DTIFIT_OUTDIR_WLS}" -m ${mask} -r ${bvecs} -b ${bvals} --wls --sse --save_tensor
 
 }
 
