3b6cfee89063dd21476e672998033eefb34587f9
Patrick Sadil <psadil@gmail.com>
2024-07-07 10:36:41 -0400

    draft v3 fmriprep app/actor

--- fmriprep_actor/reactor.py
+++ fmriprep_actor/reactor.py
@@ -1,175 +1,273 @@
-from reactors.utils import Reactor, agaveutils
 import copy
-import os
+import dataclasses
+import datetime
+import io
 import json
-import re
+import logging
+import os
+from pathlib import Path
+import math
+import typing
 
-from typing import Literal
+import ibis
+import pandas as pd
+from ibis import _
+from ibis.expr.types.relations import Table
+from tapipy import actors, errors, util
+from tapipy.tapis import Tapis, TapisResult
 
-
-# def check_metadata_file(r, file_uri):
-#     ag = r.client
-#     manifestUrl = file_uri
-#     if manifestUrl is None:
-#         try:
-#             manifestUrl = context.file_uri
-#         except Exception as e:
-#             print("No file_uri specified")
-#             exit(1)
-#     (agaveStorageSystem, dirPath, manifestFileName) = \
-#         agaveutils.from_agave_uri(uri=manifestUrl)
-#     # get the manifest and start parsing it
-#     manifestPath = dirPath + "/" + manifestFileName
-#     try:
-#         mani_file = agaveutils.agave_download_file(
-#                     agaveClient=ag,
-#                     agaveAbsolutePath=manifestPath,
-#                     systemId=agaveStorageSystem,
-#                     localFilename=manifestFileName
-#                     )
-#     except Exception as e:
-#         r.on_failure("failed to get manifest {}".format(manifestUrl), e)
-
-#     if mani_file is None:
-#         r.on_failure("failed to get manifest {}".format(manifestUrl), e)
-
-#     try:
-#         manifest = json.load(open(manifestFileName))
-#     except Exception as e:
-#         r.on_failure("failed to load manifest {}".format(manifestUrl), e)
-
-#     if 'cuff' in manifest['SeriesDescription']:
-#         job_def = copy.copy(r.settings.cuff)
-#         r.logger.info("Setting parameters for cuff image")
-#     if 'rest' in manifest['SeriesDescription']:
-#         job_def = copy.copy(r.settings.rest)
-#         r.logger.info("Setting parameters for rest image")
-#     else:
-#         print("No cuff/rest specification found for: ", manifest['SeriesDescription'])
-#         job_def = copy.copy(r.settings.rest)
-#     return job_def
+FAILUREBOT_ADDRESS_SECRET_NAME = "FAILUREBOT_ADDRESS_SECRET_NAME"
+FAILUREBOT_ADDRESS_SECRET_KEY = "FAILUREBOT_ADDRESS_SECRET_KEY"
 
 
-def submit_fmriprep(
-    r,
-    subject_id: str,
-    bids: str,
-    filename: str,
-    site: str,
-    next_step: Literal["cuff_rest", "finished"],
-    job_def,
-    image_type: Literal["anat", "cuff", "rest"],
-):
-    # Create agave client from reactor object
-    ag = r.client
-    parameters = job_def["parameters"]
-    job_def.name = "fmriprep-" + image_type + "-" + filename
-    # Define the input for the job as the file that
-    # was sent in the notificaton message
-    parameters["PARTICIPANT_LABEL"] = "sub-" + subject_id
-    parameters["BIDS_DIRECTORY"] = bids
-    if image_type in ["cuff", "rest"]:
-        parameters["FS_SUBJECTS_DIR"] = (
-            re.sub("bids", "fmriprep", bids) + "/anat/freesurfer"
+# within docker container
+JOB = Path("/opt/job.json")
+
+# on TACC
+ILOG = "/corral-secure/projects/A2CPS/shared/urrutia/imaging_report/imaging_log.csv"
+
+# can be overridden by incoming message
+MAXJOBS = 400
+
+# numbers for ls6
+N_SUBS_PER_NODE = 10
+
+SITE_LONG = {
+    "NS": "NS_northshore",
+    "UI": "UI_uic",
+    "UC": "UC_uchicago",
+    "UM": "UM_umichigan",
+    "SH": "SH_spectrum_health",
+    "WS": "WS_wayne_state",
+    "RU": "RU_rush",
+}
+
+
+@dataclasses.dataclass
+class Context(util.AttrDict):
+    raw_message: str
+    content_type: str
+    actor_repo: str
+    actor_name: str
+    actor_id: str
+    actor_dbid: str
+    execution_id: str
+    worker_id: str
+    username: str
+    state: str
+    raw_message_parse_log: str
+    message_dict: dict[str, typing.Any]
+
+
+def actors_get_client() -> Tapis:
+    """
+    Returns a pre-authenticated Tapis client using the abaco environment variables.
+    """
+    # if we have an access token, use that:
+    if token := os.environ.get("_abaco_access_token"):
+        tp = Tapis(
+            base_url=os.environ.get("_abaco_api_server", default="").strip(
+                "/"
+            ),
+            access_token=token,
+        )  # type: ignore
+    elif server := os.environ.get("_abaco_api_server"):
+        # otherwise, create a client with a fake JWT. this will only work if the actor
+        # supplies its own token to itself via a config object or the message, etc.
+        tp = Tapis(base_url=server.strip("/"), jwt="123")  # type: ignore
+    else:
+        raise errors.BaseTapyException(
+            "Unable to instantiate a Tapis client: no token found."
         )
-    job_def.parameters = parameters
-    # archivePath = os.path.dirname(os.path.dirname(os.path.normpath(bids))) \
-    #               + '/fmriprep/'+ image_type + '/' + filename
-    archivePath = (
-        re.sub("bids", "fmriprep", bids).split("/corral-secure/projects/A2CPS")[1]
-        + "/"
-        + image_type
+    return tp
+
+
+def get_ilog(client: Tapis) -> Table:
+    ilog: bytes = client.files.getContents(  # type: ignore
+        systemId="secure.corral", path=str(ILOG)
     )
-    job_def.archivePath = archivePath
-    try:
-        pipeline_config = copy.copy(r.settings.pipelines)
-        api_server = pipeline_config["api_server"]
-
-        fmriprep_nonce = os.getenv("_FMRIPREP_NONCE")
-        fmriprep_alias = pipeline_config["fmriprep_alias"]
-        fmriprep_callback = (
-            api_server
-            + "/actors/v2/"
-            + fmriprep_alias
-            + "/messages?x-nonce="
-            + fmriprep_nonce
+    return ibis.memtable(
+        pd.read_csv(
+            io.BytesIO(ilog),
+            na_values=["na", ""],
+            dtype={"subject_id": str},
         )
+    )
 
-    except Exception as e:
-        r.logger.error("Unable to generate Audit callback")
-        raise Exception(e)
 
-    notif = [
-        {
-            "event": "FINISHED",
-            "persistent": False,
-            "url": fmriprep_callback
-            + "&status=${JOB_STATUS}"
-            + "&subject_id="
-            + subject_id
-            + "&bids="
-            + bids
-            + "&filename="
-            + filename
-            + "&site="
-            + site
-            + "&next_step="
-            + next_step,
-        }
+def get_task_runlist(
+    ilog: Table, task: typing.Literal["cuff", "rest"]
+) -> Table:
+    return (
+        ilog.select(
+            "site",
+            "subject_id",
+            "visit",
+            f"fmriprep_{task}",
+            f"gift_{task}",
+        )
+        .rename(gift=f"gift_{task}")
+        .rename(fmriprep=f"fmriprep_{task}")
+        # exclude rows that were already processed
+        .filter(_.gift == 0)  # type: ignore
+        .filter(_.fmriprep == 1)  # type: ignore
+        .mutate(
+            sublong=_.site.concat(_.subject_id, _.visit),  # type: ignore
+            sitelong=_.site.cases(tuple(SITE_LONG.items())),  # type: ignore
+        )
+        .mutate(OUTPUT_DIR=_.sitelong + f"/gift_{task}/" + _.sublong)  # type: ignore
+        .mutate(
+            FMRIPREP_DIR=lambda x: "/corral-secure/projects/A2CPS/products/mris/"
+            + x.sitelong
+            + "/fmriprep/"
+            + x.sublong
+            + f"/{task}"
+            + "/fmriprep"  # type: ignore
+        )
+        .mutate(task=ibis.literal(task))
+    )
+
+
+def get_runlist(ilog: Table, maxjobs: int = MAXJOBS) -> list[tuple[str, str]]:
+    rundef = (
+        ilog.select("site", "subject_id", "visit", "bids", "fmriprep")
+        .filter(_.bids == 1)  # type: ignore
+        .filter(_.fmriprep == 0)  # type: ignore
+        .mutate(
+            sublong=_.site.concat(_.subject_id, _.visit),  # type: ignore
+            sitelong=_.site.cases(tuple(SITE_LONG.items())),  # type: ignore
+        )
+        .mutate(OUTPUT_DIR=_.sitelong + "/fmriprep/" + _.sublong)  # type: ignore
+        .mutate(
+            INPUT_DIR=lambda x: "/corral-secure/projects/A2CPS/products/mris/"
+            + x.sitelong
+            + "/bids/"
+            + x.sublong  # type: ignore
+        )
+        .order_by("visit", "subject_id")  # ensure V1 run before V3
+        .execute()
+    )
+
+    runlist = [
+        (x, y)
+        for x, y in zip(
+            rundef.INPUT_DIR.to_list(), rundef.OUTPUT_DIR.to_list()
+        )
     ]
-    job_def.notifications = notif
-    # Submit the job in a try/except block
-    try:
-        # Submit the job and get the job ID
-        job_id = ag.jobs.submit(body=job_def)["id"]
-        print(job_id)
-        print(json.dumps(job_def, indent=4))
-    except Exception as e:
-        print(json.dumps(job_def, indent=4))
-        print("Error submitting job: {}".format(e))
-        print(e.response.content)
-    return
+    return runlist[:maxjobs]
 
 
-def main():
-    """Main function"""
-    # create the reactor object
-    r = Reactor()
-    r.logger.info("Hello this is actor {}".format(r.uid))
-    # pull in reactor context
-    context = r.context  # Actor context
+def get_node_count(n_jobs: int) -> int:
+    return math.ceil(n_jobs / N_SUBS_PER_NODE)
+
+
+def get_cmd_prefix(image: str, n_jobs: int) -> str:
+    return f"ibrun -n 1 apptainer run {image} --help && ibrun -n {n_jobs}"
+
+
+def set_app_arg(job: dict, arg_pos: int, name: str, arg: str) -> dict:
+    job2 = copy.deepcopy(job)
+    job2.get("parameterSet").get("appArgs")[arg_pos] = {"name": name, "arg": arg}  # type: ignore
+    return job2
+
+
+def set_key_value(job: dict, key: str, value: int | str | None = None) -> None:
+    if value:
+        job[key] = value
+
+
+def get_failurebot_url(client) -> str:
+    token: TapisResult = client.sk.readSecret(  # type: ignore
+        secretType="user",
+        secretName=FAILUREBOT_ADDRESS_SECRET_NAME,
+        tenant=os.environ.get("_abaco_api_server")
+        .split(".")[0]  # type: ignore
+        .split("/")[-1],
+        user=client.actors.get_actor(
+            actor_id=os.environ.get("_abaco_actor_id")
+        ).owner,
+    )
+    url: str | None = token.get("secretMap").get(FAILUREBOT_ADDRESS_SECRET_KEY)  # type: ignore
+    if url is None:
+        msg = f"unable to find {FAILUREBOT_ADDRESS_SECRET_KEY} in secretMap"
+        raise AssertionError(msg)
+
+    return url
+
+
+def set_subscription_url(job: dict, arg: str) -> dict:
+    job2 = copy.deepcopy(job)
+    job2.get("subscriptions")[0].get("deliveryTargets")[0].update(  # type: ignore
+        {"deliveryAddress": arg}
+    )
+    return job2
+
+
+def main() -> None:
+    context: Context = actors.get_context()  # type: ignore
     print(json.dumps(context, indent=4))
-    # archivePath=context.archivePath
-    subject_id = context.subject_id
-    filename = context.filename
-    bids = context.bids
-    message = context.message_dict
-    site = context.site
+    client = actors_get_client()
 
-    next_step = context.next_step
-    if message["status"] != "FINISHED":
-        exit(0)
+    ilog = get_ilog(client=client)
 
-    if next_step == "anat":
-        next_step = "cuff_rest"
-        job_def = copy.copy(r.settings.anat)
-        submit_fmriprep(r, subject_id, bids, filename, site, next_step, job_def, "anat")
-    elif next_step == "cuff_rest":
-        next_step = "finished"
-        job_def = copy.copy(r.settings.cuff)
-        submit_fmriprep(r, subject_id, bids, filename, site, next_step, job_def, "cuff")
-        job_def = copy.copy(r.settings.rest)
-        submit_fmriprep(r, subject_id, bids, filename, site, next_step, job_def, "rest")
-    # tapis_jobId=message['id']
-    # if m['status'] != 'FINISHED':
-    #     r.on_failure("Tapis jobId={} has status {}.".format(
-    #         tapis_jobId, m['status']) + "Skipping validation.")
-    #     exit(0)
-    # print(message)
-    # check the file_uri from the message
-    # depending on the file_uri, set fmriprp parameters for a rest or cuff fmri
-    # job_def = check_metadata_file(r, file_uri)
-    return
+    runlist = get_runlist(
+        ilog=ilog, maxjobs=context.message_dict.get("maxjobs", MAXJOBS)
+    )
+    if not len(runlist):
+        logging.warning("Did not find any jobs to submit")
+        return
+
+    with open(JOB, "r") as f:
+        job = json.load(f)
+
+    n_jobs = len(runlist)
+    n_nodes = get_node_count(n_jobs)
+    job = set_app_arg(
+        job,
+        0,
+        name="INPUT_DIRS",
+        arg="--input-dirs " + " ".join(x[0] for x in runlist),
+    )
+    job = set_app_arg(
+        job,
+        1,
+        name="OUTPUT_DIRS",
+        arg="--output-dirs " + " ".join(x[1] for x in runlist),
+    )
+
+    set_key_value(
+        job, key="maxMinutes", value=context.message_dict.get("maxMinutes")
+    )
+    set_key_value(
+        job,
+        key="name",
+        value=f"gift-{datetime.datetime.today().strftime('%Y-%m-%d')}",
+    )
+
+    image = client.apps.getApp(
+        appId=job["appId"], appVersion=job["appVersion"]
+    ).containerImage
+    set_key_value(
+        job, key="cmdPrefix", value=get_cmd_prefix(n_jobs=n_jobs, image=image)
+    )
+
+    # corresponds to SBATCH option -N,--nodes, SLURM_JOB_NUM_NODES
+    set_key_value(job, key="nodeCount", value=n_nodes)
+
+    # corresponds to SBATCH option -n,--ntask, SLURM_NPROCS, SLURM_NTASKS
+    # all nodes will have all cores available, but this needs to be set for ibrun
+    set_key_value(job, key="coresPerNode", value=N_SUBS_PER_NODE)
+
+    failurebot_url = get_failurebot_url(client=client)
+    set_subscription_url(job, arg=failurebot_url)
+
+    print(json.dumps(job, indent=4))
+
+    try:
+        submitted = client.jobs.submitJob(**job)  # type: ignore
+        print(submitted.uuid)
+    except Exception as e:
+        logging.error(f"encountered while trying to submit job: {e}")
 
 
 if __name__ == "__main__":
