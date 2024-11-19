edef5ddab22611220bebe558d94276b80d8fb423
Patrick Sadil <psadil@gmail.com>
2024-11-18 19:48:34 -0500

    continued name adjustments

--- gift_app/assets/entrypoint.py
+++ gift_app/assets/entrypoint.py
@@ -72,7 +72,7 @@ if __name__ == "__main__":
                         Path(input_dir).relative_to(
                             "/corral-secure/projects/A2CPS/products/mris"
                         )
-                    ).replace("/fmriprep/", "gift")
+                    ).replace("/fmriprep/", "/gift/")
                 ).parent
             )
     else:
