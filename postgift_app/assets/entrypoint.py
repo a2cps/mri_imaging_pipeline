bec0fc073c24fb300c1167de4fa24963a9677fc6
Patrick Sadil <psadil@gmail.com>
2025-06-09 09:40:42 -0400

    create new app/actor for converting gift to tabular format

---
+++ postgift_app/assets/entrypoint.py
@@ -0,0 +1,57 @@
+import argparse
+import asyncio
+import shutil
+from pathlib import Path
+
+from biomarkers.entrypoints import postgift, tapismpi
+from mpi4py import MPI
+
+tapismpi.configure_mpi_logger()
+
+
+async def main(ins: list[Path], outs: list[Path]) -> None:
+    await postgift.PostGIFTEntrypoint(
+        outs=outs,
+        ins=ins,
+        stage_ignore_patterns=shutil.ignore_patterns(
+            "*bold.nii.gz", "network_summary", "*gica_results"
+        ),
+    ).run()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-dirs", nargs="+", type=Path, required=True)
+    parser.add_argument("--output-dirs", nargs="+", type=Path)
+
+    args = parser.parse_args()
+    usize = MPI.COMM_WORLD.Get_size()
+
+    if args.output_dirs is None:
+        output_dirs = []
+        for input_dir in args.input_dirs:
+            output_dirs.append(
+                Path(
+                    str(
+                        Path(input_dir).relative_to(
+                            "/corral-secure/projects/A2CPS/products/mris"
+                        )
+                    ).replace("/gift/", "/postgift/")
+                ).parent
+            )
+    else:
+        output_dirs = args.output_dirs
+
+    if not (n_input := len(args.input_dirs)) == usize:
+        msg = f"Length of input_dirs must equal usize but found {n_input=}, {usize=}"
+        raise AssertionError(msg)
+
+    if not (n_output := len(output_dirs)) == usize:
+        msg = f"Length of output_dirs must equal usize but found {n_output=}, {usize=}"
+        raise AssertionError(msg)
+
+    if not len(output_dirs) == len(set(output_dirs)):
+        msg = "Output directories must be unique"
+        raise AssertionError(msg)
+
+    asyncio.run(main(ins=args.input_dirs, outs=output_dirs))
