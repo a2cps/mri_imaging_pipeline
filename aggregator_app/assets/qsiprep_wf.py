c0cfd7b8dac0aa63f6dc50f22e825a8317c83b86
Patrick Sadil <psadil@gmail.com>
2025-10-14 09:52:44 -0400

    aggregate new qsiprep outputs

--- aggregator_app/assets/qsiprep_wf.py
+++ aggregator_app/assets/qsiprep_wf.py
@@ -4,25 +4,29 @@ import utils
 from biomarkers import utils as bu
 
 
-def copy(outdir: Path, inroot: Path) -> None:
-    bu.mkdir_recursive(outdir / "qsiprep-V1")
-    bu.mkdir_recursive(outdir / "qsiprep-V3")
+def copy(outdir: Path, inroot: Path, job: str = "qsiprep") -> None:
+    bu.mkdir_recursive(outdir / f"{job}-V1")
+    bu.mkdir_recursive(outdir / f"{job}-V3")
 
-    for src in inroot.glob("qsiprep/*/qsiprep/sub*"):
+    for src in inroot.glob(f"{job}/*/qsiprep/sub*"):
         ses = bu.get_ses_from_sublong(src)
         if src.is_file():
             utils.symlink_if_needed(
                 src,
-                outdir / f"qsiprep-{ses}" / src.name,
+                outdir / f"{job}-{ses}" / src.name,
             )
         else:
-            utils.mergetree_overwrite(src, outdir / f"qsiprep-{ses}" / src.name)
+            utils.mergetree_overwrite(src, outdir / f"{job}-{ses}" / src.name)
 
-    for src in inroot.glob("qsiprep/*/eddyqc"):
+    for src in inroot.glob(f"{job}/*/eddyqc"):
         sub = bu.get_sub_from_sublong(src)
         ses = bu.get_ses_from_sublong(src)
 
-        out_subses = outdir / "eddyqc" / f"sub-{sub}" / f"ses-{ses}"
+        if "2" in job:
+            eqc = "eddyqc2"
+        else:
+            eqc = "eddyqc"
+        out_subses = outdir / eqc / f"sub-{sub}" / f"ses-{ses}"
         bu.mkdir_recursive(out_subses)
 
         utils.mergetree_overwrite(src, out_subses)
