487773b686a92ef961f40964b8ed61044b28d611
Patrick Sadil <psadil@gmail.com>
2026-08-25 13:15:26 -0400

    fix: ensure parent dir is created (should already be true, but just in case)

--- aggregator_app/assets/synthstrip_wf.py
+++ aggregator_app/assets/synthstrip_wf.py
@@ -68,6 +68,6 @@ def copyv4(outdir: Path, inroot: Path) -> None:
 def make_toplevel(outdir: Path, inroot: Path) -> None:
 
     pl.scan_csv(f"{inroot}/*/synthstrip/*/volumes.tsv", separator="\t").sink_csv(
-        outdir / "volumes.tsv", separator="\t"
+        outdir / "volumes.tsv", separator="\t", mkdir=True
     )
     (outdir / "volumes.json").write_text(json.dumps(VOLUMES, indent=2, sort_keys=True))
